8e85b96f8bcc30219100d15fa8e27807b7464ae8
Maxim Khotsevich <mkhotsevich@sberbank.ru>
2022-06-21 14:39:47 +0300

    change ignore

--- dist/index.js
+++ dist/index.js
@@ -1,27 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -58,17 +35,16 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.read = exports.convert = exports.downloadFromS3 = void 0;
+exports.pklToJson = void 0;
+var fs = require("fs");
+var path = require("path");
 var child_process_1 = require("child_process");
-var path = __importStar(require("path"));
-var fs = __importStar(require("fs"));
-// @ts-ignore
-var big_json_1 = __importDefault(require("big-json"));
-function isExists(filePath) {
+function isValidExtname(filePath, ext) {
+    var fileExt = path.extname(filePath);
+    return fileExt === ext;
+}
+function isFileExists(filePath) {
     return __awaiter(this, void 0, void 0, function () {
         var _a;
         return __generator(this, function (_b) {
@@ -87,148 +63,81 @@ function isExists(filePath) {
         });
     });
 }
-function isExtname(filePath, ext) {
-    var fileExt = path.extname(filePath);
-    return fileExt === ext;
-}
-function downloadFromS3(_a) {
-    var _this = this;
-    var accessKeyId = _a.accessKeyId, secretAccessKey = _a.secretAccessKey, endpointUrl = _a.endpointUrl, bucketName = _a.bucketName, pathsToLoad = _a.pathsToLoad, directoryPath = _a.directoryPath, destinationPath = _a.destinationPath;
-    return new Promise(function (resolve, reject) {
-        var python = (0, child_process_1.spawn)("/usr/bin/env", [
-            "python3",
-            path.resolve(__dirname, "./download.py"),
-            accessKeyId,
-            secretAccessKey,
-            endpointUrl,
-            bucketName,
-            pathsToLoad.join(","),
-            directoryPath,
-            destinationPath,
-        ]);
-        python.stderr.on("data", function (error) { return reject(error.toString()); });
-        python.on("close", function () { return __awaiter(_this, void 0, void 0, function () {
-            var newPaths, rmDir, e_1;
-            var _this = this;
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0:
-                        _a.trys.push([0, 3, , 4]);
-                        return [4 /*yield*/, Promise.all(pathsToLoad.map(function (p) { return __awaiter(_this, void 0, void 0, function () {
-                                var oldPath, newPath;
-                                return __generator(this, function (_a) {
-                                    switch (_a.label) {
-                                        case 0:
-                                            oldPath = path.resolve(destinationPath, p);
-                                            newPath = path.resolve(destinationPath, path.basename(p));
-                                            return [4 /*yield*/, fs.promises.rename(oldPath, newPath)];
-                                        case 1:
-                                            _a.sent();
-                                            return [2 /*return*/, newPath];
-                                    }
-                                });
-                            }); }))];
-                    case 1:
-                        newPaths = _a.sent();
-                        rmDir = path.resolve(__dirname, directoryPath.split("/")[0]);
-                        return [4 /*yield*/, fs.promises.rm(rmDir, { force: true, recursive: true })];
-                    case 2:
-                        _a.sent();
-                        resolve(newPaths);
-                        return [3 /*break*/, 4];
-                    case 3:
-                        e_1 = _a.sent();
-                        reject(e_1.message);
-                        return [3 /*break*/, 4];
-                    case 4: return [2 /*return*/];
-                }
-            });
-        }); });
-    });
-}
-exports.downloadFromS3 = downloadFromS3;
-function convertToJson(fromPkl, toJson) {
+function convertToJson(pklPath, jsonPath) {
     return new Promise(function (resolve, reject) {
         var python = (0, child_process_1.spawn)("/usr/bin/env", [
             "python3",
             path.resolve(__dirname, "./convert.py"),
-            fromPkl,
-            toJson,
+            pklPath,
+            jsonPath,
         ]);
         python.stderr.on("data", function () {
             return reject("Error in python script while converting");
         });
-        python.on("close", function () { return resolve(); });
+        python.on("close", function () { return resolve(jsonPath); });
     });
 }
-function convert(fromPkl, toJson) {
-    var _this = this;
-    return new Promise(function (resolve, reject) { return __awaiter(_this, void 0, void 0, function () {
-        var e_2;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    _a.trys.push([0, 4, , 5]);
-                    if (!isExtname(fromPkl, ".pkl")) {
-                        throw new Error("".concat(fromPkl, " is not .pkl file"));
-                    }
-                    if (!isExtname(toJson, ".json")) {
-                        throw new Error("".concat(toJson, " is not .json file"));
-                    }
-                    return [4 /*yield*/, isExists(fromPkl)];
-                case 1:
-                    if (!(_a.sent())) {
-                        throw new Error("Can not find ".concat(fromPkl));
-                    }
-                    return [4 /*yield*/, isExists(toJson)];
-                case 2:
-                    if (_a.sent()) {
-                        return [2 /*return*/, resolve(toJson)];
-                    }
-                    return [4 /*yield*/, convertToJson(fromPkl, toJson)];
-                case 3:
-                    _a.sent();
-                    return [2 /*return*/, resolve(toJson)];
-                case 4:
-                    e_2 = _a.sent();
-                    reject(e_2.message);
-                    return [3 /*break*/, 5];
-                case 5: return [2 /*return*/];
-            }
+function convertToPkl(jsonPath, pklPath) {
+    return new Promise(function (resolve, reject) {
+        var python = (0, child_process_1.spawn)("/usr/bin/env", [
+            "python3",
+            path.resolve(__dirname, "./reconvert.py"),
+            jsonPath,
+            pklPath,
+        ]);
+        python.stderr.on("data", function () {
+            return reject("Error in python script while converting");
         });
-    }); });
+        python.on("close", function () { return resolve(pklPath); });
+    });
 }
-exports.convert = convert;
-function read(jsonPath) {
-    var _this = this;
-    return new Promise(function (resolve, reject) { return __awaiter(_this, void 0, void 0, function () {
-        var jsonData_1, readStream, parseStream, e_3;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    _a.trys.push([0, 2, , 3]);
-                    if (!isExtname(jsonPath, ".json")) {
-                        throw new Error("".concat(jsonPath, " is not .json file"));
-                    }
-                    return [4 /*yield*/, isExists(jsonPath)];
-                case 1:
-                    if (!(_a.sent())) {
-                        throw new Error("Can not find ".concat(jsonPath));
-                    }
-                    readStream = fs.createReadStream(jsonPath);
-                    parseStream = big_json_1.default.createParseStream();
-                    readStream.pipe(parseStream);
-                    parseStream.on("data", function (data) { return (jsonData_1 = data); });
-                    parseStream.on("error", function () { return reject("Error during reading json file"); });
-                    parseStream.on("end", function () { return resolve(jsonData_1); });
-                    return [3 /*break*/, 3];
-                case 2:
-                    e_3 = _a.sent();
-                    reject(e_3.message);
-                    return [3 /*break*/, 3];
-                case 3: return [2 /*return*/];
-            }
+exports.pklToJson = {
+    convert: function (pklPath, jsonPath) {
+        return __awaiter(this, void 0, void 0, function () {
+            var isPklFileExists;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        if (!isValidExtname(pklPath, ".pkl")) {
+                            throw new Error("".concat(pklPath, " is not .pkl file"));
+                        }
+                        if (!isValidExtname(jsonPath, ".json")) {
+                            throw new Error("".concat(jsonPath, " is not .json file"));
+                        }
+                        return [4 /*yield*/, isFileExists(pklPath)];
+                    case 1:
+                        isPklFileExists = _a.sent();
+                        if (!isPklFileExists) {
+                            throw new Error("Can not find ".concat(pklPath));
+                        }
+                        return [4 /*yield*/, convertToJson(pklPath, jsonPath)];
+                    case 2: return [2 /*return*/, _a.sent()];
+                }
+            });
         });
-    }); });
-}
-exports.read = read;
+    },
+    reconvert: function (jsonPath, pklPath) {
+        return __awaiter(this, void 0, void 0, function () {
+            var isJsonFileExists;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        if (!isValidExtname(pklPath, ".pkl")) {
+                            throw new Error("".concat(pklPath, " is not .pkl file"));
+                        }
+                        if (!isValidExtname(jsonPath, ".json")) {
+                            throw new Error("".concat(jsonPath, " is not .json file"));
+                        }
+                        return [4 /*yield*/, isFileExists(jsonPath)];
+                    case 1:
+                        isJsonFileExists = _a.sent();
+                        if (!isJsonFileExists) {
+                            throw new Error("Can not find ".concat(jsonPath));
+                        }
+                        return [4 /*yield*/, convertToPkl(jsonPath, pklPath)];
+                    case 2: return [2 /*return*/, _a.sent()];
+                }
+            });
+        });
+    },
+};
